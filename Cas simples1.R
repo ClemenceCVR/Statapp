955e717b955ee0f18d915ae04f33632358a63a5f
cchevrier <clemence.chevrier@ensae.fr>
2024-03-04 20:41:41 +0000

    Renommer var

--- Cas simples1.R
+++ Cas simples1.R
@@ -17,47 +17,30 @@ time <- 1:n_periods
 
 # Generate synthetic data
 data2 <- data.frame(
-  Group = rep(c("Group 1", "Group 2", "Group 3"), each = n_per_group * n_periods),
-  Individual = rep(rep(1:n_per_group, each = n_periods), times = 3),
-  Time = rep(time, times = 3 * n_per_group),
+  G = rep(c("1", "2", "3"), each = n_per_group * n_periods),
+  id = rep(rep(1:n_per_group, each = n_periods), times = 3),
+  period = rep(time, times = 3 * n_per_group),
   stringsAsFactors = FALSE
 )
 
-# data = data %>% group_by(Individual) %>% mutate(Treatment = ifelse(Time > 50 & Group == "Group 1", 1, 0),
-#                                                 Outcome = rnorm(2 * n_periods, mean = ifelse(Treatment == 1, 2+0.5*Time+5, -5+0.5*Time))) %>% distinct()
 
-data2 = data2 %>% group_by(Individual) %>% mutate(Treatment = ifelse((Time > 50 & Group == "Group 1") | (Time > 75 & Group == "Group 2"), 1, 0),
-                                                Outcome = rnorm(3 * n_periods, mean = case_when(
-                                                  Treatment == 1 & Group== "Group 1" ~ -5+0.5*Time+10+0.3*Time,
-                                                  Treatment == 0 & Group== "Group 1" ~ -5+0.5*Time,
-                                                  Treatment == 1 & Group== "Group 2" ~ -15+0.5*Time+10+0.3*Time,
-                                                  Treatment == 0 & Group== "Group 2" ~ -15+0.5*Time,
-                                                  TRUE ~ 0.5*Time))) %>% distinct()
+data2 = data2 %>% group_by(id) %>% mutate(treat = ifelse((period > 50 & G == "1") | (period > 75 & G == "2"), 1, 0),
+                                          Y = rnorm(3 * n_periods, mean = case_when(
+                                            treat == 1 & G== "1" ~ -5+0.5*period+10+0.3*period,
+                                            treat == 0 & G== "1" ~ -5+0.5*period,
+                                            treat == 1 & G== "2" ~ -15+0.5*period+10+0.3*period,
+                                            treat == 0 & G== "2" ~ -15+0.5*period,
+                                            TRUE ~ 0.5*period))) %>% distinct()
 
 # Plot the simulated data
-ggplot(data2, aes(x = Time, y = Outcome, color = Group, linetype = factor(Treatment))) +
+ggplot(data2, aes(x = period, y = Y, color = G, linetype = factor(treat))) +
   geom_point() +
-  labs(x = "Time", y = "Outcome", color = "Group", linetype = "Treatment") +
+  labs(x = "period", y = "Y", color = "G", linetype = "treat") +
   theme_minimal()
 
 # Estimate DiD parameters using linear regression
-diD_model <- lm(Outcome ~ Treatment + Time + Treatment:Time + Group, data2 = data2)
+diD_model <- lm(Y ~ treat + period + treat:period + G, data = data2)
 
 # Display DiD model summary
 summary(diD_model)
 
-#Mettre data2 sous la forme des bases de données du départ
-
-#On change le nom des colonnes
-data3 <- data2 %>%
-  rename(G = Group,
-         id = Individual,
-         period = Time,
-         treat = Treatment,
-         Y = Outcome)
-
-unique(data3$G) #on a Group1, Group2, Group3
-#on remplace par les chiffres
-data3$G <- gsub("Group 1", "1", data3$G)
-data3$G <- gsub("Group 2", "2", data3$G)
-data3$G <- gsub("Group 3", "3", data3$G)
